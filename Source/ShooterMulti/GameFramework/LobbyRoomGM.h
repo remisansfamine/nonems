c3ba04bb4cab1c70cfc931b21364c3ab46663048
s.legrand <s.legrand@student.isartdigital.com>
2022-12-02 16:43:35 +0100

    [lobby] Added base classes for lobby management

---
+++ Source/ShooterMulti/GameFramework/LobbyRoomGM.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "LobbyRoomGM.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTERMULTI_API ALobbyRoomGM : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
